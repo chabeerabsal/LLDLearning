19b714619b8122c8b5eb34754f035f8742b7defe
chabeerabsal <chabeerabsal@gmail.com>
2025-05-28 10:20:50 +0530

    LLDlearningMessyCode

---
+++ src/Main.java
@@ -0,0 +1,114 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+
+    public Board start(){
+        return new Board();
+    }
+    public void move(Board board, Player player,Move move){
+
+    }
+    public GameResult isComplete(Board board){
+         if(board instanceof TicTacToeBoard)
+         {
+             TicTacToeBoard board1 = (TicTacToeBoard) board;
+             boolean rowComplete=true;
+             String firstCharacter="_";
+             for(int i=0;i<3;i++)
+             {
+                 rowComplete=true;
+
+                 for(int j=0;j<3;j++)
+                 {
+                     firstCharacter=board1.cells[i][0];
+                     if(!board1.cells[i][j].equals(firstCharacter)) {
+                         rowComplete = false;
+                         break;
+                     }
+                 }
+                 if(rowComplete)
+                     return new GameResult(true,firstCharacter);
+
+               boolean  colComplete=true;
+
+                 for(int j=0;j<3;j++)
+                 {
+                     firstCharacter=board1.cells[0][i];
+                     if(!board1.cells[j][i].equals(firstCharacter)) {
+                         colComplete = false;
+                         break;
+                     }
+                 }
+                 if(colComplete)
+                     return new GameResult(true,firstCharacter);
+
+                 boolean  diagComplete=true;
+
+                 for(int j=0;j<3;j++)
+                 {
+                     firstCharacter=board1.cells[0][0];
+                     if(!board1.cells[i][i].equals(firstCharacter)) {
+                         diagComplete = false;
+                         break;
+                     }
+                 }
+                 if(diagComplete)
+                     return new GameResult(true,firstCharacter);
+                 boolean  revdiagComplete=true;
+
+                 for(int j=0;j<3;j++)
+                 {
+                     firstCharacter=board1.cells[0][2];
+                     if(!board1.cells[i][2-i].equals(firstCharacter)) {
+                         colComplete = false;
+                         break;
+                     }
+                 }
+                 if(colComplete)
+                     return new GameResult(true,firstCharacter);
+             }
+          int count=0;
+             for(int i=0;i<3;i++)
+             {
+                 for(int j=0;j<3;j++)
+                 {
+                     if(board1.cells[i][j]!=null)
+                         count++;
+
+                 }
+             }
+             if(count==9)
+                 return new GameResult(true,"_");
+             else
+                 return new GameResult(false,"_");
+
+         }
+         return new GameResult(false,"_");
+    }
+}
+class Board {
+
+}
+class TicTacToeBoard extends Board {
+      String[][] cells=new String[3][3];
+}
+class Player {
+
+}
+class Move {
+
+}
+class GameResult{
+   boolean isOver;
+   String winner;
+
+    public GameResult(boolean isOver, String winner) {
+        this.isOver = isOver;
+        this.winner = winner;
+    }
+}

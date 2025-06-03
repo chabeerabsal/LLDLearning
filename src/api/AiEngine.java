6ce508f27de057d77a1e3f6ca7238938adf87c0e
chabeerabsal <chabeerabsal@gmail.com>
2025-06-03 08:44:05 +0530

    Single Responcibility princible

--- src/api/AiEngine.java
+++ src/api/AiEngine.java
@@ -11,6 +11,7 @@ public class AiEngine {
         if(board instanceof TicTacToeBoard)
         {
             TicTacToeBoard board1 = (TicTacToeBoard) board;
+            //simple move by computer
             for(int i=0;i<3;i++)
             {
                 for(int j=0;j<3;j++)

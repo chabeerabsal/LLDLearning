ca5a559503f3b364947e79f2cd2f1aab4270867d
chabeerabsal <chabeerabsal@gmail.com>
2025-06-06 05:57:48 +0530

    Single Responcibility princible

--- src/api/AiEngine.java
+++ src/api/AiEngine.java
@@ -13,17 +13,44 @@ public class AiEngine {
             TicTacToeBoard board1 = (TicTacToeBoard) board;
             //simple move by computer
 
-
-            for(int i=0;i<3;i++)
-            {
-                for(int j=0;j<3;j++)
-                {
-                    if(board1.getcells(i,j)==null)
-                        return new Move(new Cells(i,j),computer);
-
-                }
+            Move suggestion=null;
+            if(isStarting(board1,4)) {
+                suggestion=getBasicMove(board1, computer);
             }
+//            else{
+//                return smartMove(board1,computer);
+//            }
+            if(suggestion!=null)
+                return suggestion;
             throw new IllegalStateException();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isStarting(TicTacToeBoard board1, int threshold) {
+        int count=0;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(board1.getcells(i,j)==null)
+                    count++;
+
+            }
+        }
+        return count<threshold;
+
+    }
+
+    private Move getBasicMove(TicTacToeBoard board1, Player computer) {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(board1.getcells(i,j)==null)
+                    return new Move(new Cells(i,j),computer);
+
+            }
         }
         throw new IllegalArgumentException();
     }
